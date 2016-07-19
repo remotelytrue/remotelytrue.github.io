5b27ced6036bf0801628c0147b86deae5f19216d
Phillip Steindel <pstein@bu.edu>
2016-07-19 15:58:44 -0400

    remove console.log command

--- static/js/super-search.js
+++ static/js/super-search.js
@@ -109,7 +109,6 @@
             return;
         }
         searchResultsEl.style.offsetWidth;
-        console.log(posts)
 
         var matchingPosts = posts.filter(function (post) {
             if ((post.title + '').toLowerCase().indexOf(currentInputValue) !== -1 || (post.description + '').toLowerCase().indexOf(currentInputValue) !== -1) {
